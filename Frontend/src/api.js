a8102503e9ffb94caf0143666749956251934056
Mahesh0843 <singulurimahesh123@gmail.com>
2025-12-17 15:49:19 +0530

    Updaed backed

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const backendURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:7777/";
+
 const api = axios.create({
-  baseURL: "http://localhost:7777/",
+  baseURL: backendURL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json"
